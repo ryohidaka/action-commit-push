a4320c83fb9a627b32f63ee81e9867443e1d4e50
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-10-20 00:50:13 +0900

    fix: Ensure proper handling of filenames with spaces during git add

--- scripts/check_diff.sh
+++ scripts/check_diff.sh
@@ -5,14 +5,14 @@ set -e # Exit immediately if any command exits with a non-zero status
 files="${1}"
 
 # Add the specified files (file/directory) to the git staging area
-git add "$files"
+echo "$files" | xargs -I {} git add "{}"
 
 # Check if there are any staged changes; exit if none
 if git diff --cached --exit-code >/dev/null; then
-    echo "changes=false" >>$GITHUB_OUTPUT
-    echo "::warning::No changes to commit. Skipping commit and push."
+	echo "changes=false" >>$GITHUB_OUTPUT
+	echo "::warning::No changes to commit. Skipping commit and push."
 else
-    echo "changes=true" >>$GITHUB_OUTPUT
-    echo "Staged files with changes:"
-    git diff --cached --name-only
+	echo "changes=true" >>$GITHUB_OUTPUT
+	echo "Staged files with changes:"
+	git diff --cached --name-only
 fi
