7eff396662b68b2ad8ca06f7efe2280632e9c584
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-01-02 19:59:05 +0900

    fix: Ensure branch tracking information is set before rebase and push.

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e # Exit immediately if any command exits with a non-zero status
 
+# Use the provided branch name or fallback to the current branch
+BRANCH_NAME=${2:-$(git rev-parse --abbrev-ref HEAD)}
+
 # Check if rebase input is true
 if [ "$1" == "true" ]; then
-    echo "::debug::Rebase is true. Running git pull --rebase."
-    git pull --rebase
-fi
+    echo "::debug::Rebase is true. Ensuring branch tracking information is set."
+    # Ensure the branch is tracking the correct remote branch
+    git branch --set-upstream-to=origin/"$BRANCH_NAME" || true
 
-# Set the branch to push if not provided as an argument
-BRANCH_NAME=${2:-${GITHUB_REF##*/}} # Use input branch or default to current branch
+    echo "::debug::Running git pull --rebase."
+    git pull --rebase origin "$BRANCH_NAME"
+fi
 
 # Push the committed changes to the remote repository on the specified branch
 git push origin "$BRANCH_NAME"
